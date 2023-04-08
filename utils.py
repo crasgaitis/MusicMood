d84e7deb59414510f8daf2f445bdeb946468ea8f
crasgaitis <crasgaitis@gmail.com>
2023-04-08 12:44:27 -0700

    connecting vader to major/minor

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from music21 import *
 import random
+import numpy as np
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 def make_music(key_input):
 
@@ -29,6 +31,30 @@ def get_key(type):
     key = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
   return random.choice(key)
 
+def get_ma_mi(set):
+
+  score_avg = np.mean(set['mh_state'])
+
+  if score_avg > -0.3:
+    return 'major'
+  else:
+    return 'minor'
+
+def analyze(set):
+
+  analyzer = SentimentIntensityAnalyzer()
+
+  sentiment_scores = []
+  for message in set['text']:
+      score = analyzer.polarity_scores(str(message))
+      sentiment_scores.append(score['compound'])
+
+  set['mh_state'] = sentiment_scores
+
+
 
 # test
+
+analyze(user_set)
+
 make_music(get_key('major'))
