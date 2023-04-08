720aaf64224a21f69e25bdaada914c4861ff1d86
crasgaitis <crasgaitis@gmail.com>
2023-04-08 11:47:27 -0700

    utils for music gen

---
+++ utils.py
@@ -0,0 +1,34 @@
+from music21 import *
+import random
+
+def make_music(key_input):
+
+  key_str = key_input
+  key_obj = key.Key(key_str)
+  time_signature = meter.TimeSignature('4/4')
+
+  # melody: random notes
+  melody = stream.Stream()
+  melody.append(key_obj)
+  melody.append(time_signature)
+
+  for i in range(8):
+      note_name = random.choice(scale.MajorScale(key_str).getPitches() + scale.MelodicMinorScale(key_str).getPitches())
+      note_obj = note.Note(note_name)
+      note_obj.duration = duration.Duration(random.choice([0.25, 0.5, 1, 2]))
+      melody.append(note_obj)
+
+  # create MIDI file
+  midi_filename = 'output.mid'
+  melody.write('midi', fp=midi_filename)
+
+def get_key(type):
+  if type == "major":
+    key = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+  else:
+    key = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+  return random.choice(key)
+
+
+# test
+make_music(get_key('major'))
