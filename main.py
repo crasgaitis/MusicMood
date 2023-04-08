d646c1f63a09e9e389934c5740eb4bb8fbebc44b
crasgaitis <crasgaitis@gmail.com>
2023-04-08 13:06:39 -0700

    attempt at sound

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ from utils import make_music, get_key, get_ma_mi, analyze
 import streamlit as st  
 
 st.write('please work')
+
+try:
+    user_set = st.file_uploader("upload file", type={"csv"})
+    analyze(user_set)    
+    make_music(get_key(get_ma_mi(user_set)))
+    
+    audio_file = open('output.mid', 'rb')
+    audio_bytes = audio_file.read()
+    
+    st.audio(audio_bytes, format='audio/ogg')
+    
+except:
+    pass
